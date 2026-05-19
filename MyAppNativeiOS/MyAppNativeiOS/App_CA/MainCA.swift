564c0bd0e00b7de8adec5617c2d18ad68438c45b
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-05-19 09:26:45 -0300

    DEV - ADD MainCA.swift

---
+++ MyAppNativeiOS/MyAppNativeiOS/App_CA/MainCA.swift
@@ -0,0 +1,15 @@
+//
+//  MainCA.swift
+//  MyAppNativeiOS
+//
+//  Created by Daniel Sanabria on 19/05/26.
+//  Copyright © 2026 SAP. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainCA: View {
+    var body: some View {
+        Text("This is the Main Clean Architecture View")
+    }
+}
