be6ebedade0a428b8cfa3f6b749f76406b622f40
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-05-19 14:42:38 -0300

    DEV - CustomerRepositoryMock + RespositoryError - CustomerRepository.swift

--- MyAppNativeiOS/MyAppNativeiOS/App_CA/Domain/Repositories/CustomerRepository.swift
+++ MyAppNativeiOS/MyAppNativeiOS/App_CA/Domain/Repositories/CustomerRepository.swift
@@ -15,3 +15,34 @@ protocol CustomerRepository {
     func update(_ customer: Customer) async throws
     func delete(id: String) async throws
 }
+
+final class CustomerRepositoryMock: CustomerRepository {
+    var customers: [Customer] = []
+    
+    func fetchAll() async throws -> [Customer] {
+        customers
+    }
+    func fetchById(_ id: String) async throws -> Customer? {
+        customers.first { $0.id == id }
+    }
+    func create(_ customer: Customer) async throws {
+        customers.append(customer)
+    }
+    func update(_ customer: Customer) async throws {
+        guard let index = customers.firstIndex(where: { $0.id == customer.id }) else {
+                throw RepositoryError.notFound
+            }
+        customers[index] = customer
+    }
+    func delete(id: String) async throws {
+        guard let index = customers.firstIndex(where: { $0.id == id }) else {
+                throw RepositoryError.notFound
+            }
+        customers.remove(at: index)
+    }
+
+}
+
+enum RepositoryError: Error {
+    case notFound
+}
