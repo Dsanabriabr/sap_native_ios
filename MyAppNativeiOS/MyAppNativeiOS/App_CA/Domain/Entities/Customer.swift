f208d50daa33fd15234b89a0000456600101906c
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-05-19 12:00:25 -0300

    DEV - Add entity Customer.swift

---
+++ MyAppNativeiOS/MyAppNativeiOS/App_CA/Domain/Entities/Customer.swift
@@ -0,0 +1,27 @@
+//
+//  Customer.swift
+//  MyAppNativeiOS
+//
+//  Created by Daniel Sanabria on 19/05/26.
+//  Copyright © 2026 SAP. All rights reserved.
+//
+
+import MyAppMDKDataFmwk
+
+struct Address: Equatable {
+    var street: String?
+    var houseNumber: String?
+    var city: String?
+    var postalCode: String?
+    var country: String?
+}
+
+struct Customer: Equatable, Identifiable {
+    let id: String
+    
+    var firstName: String
+    var lastName: String
+    var email: String?
+    var phone: String?
+    var address: Address?
+}
